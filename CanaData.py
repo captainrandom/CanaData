5ea19a4626c1025fe7b685f017ae611f6808c6dc
Don M <demnod@gmail.com>
2019-10-25 20:55:58 -0700

    Adding US + Canada Slugs
    Adding functionality to figure out all State slugs in US + Canada to make searching easier.
    
    Removed the mylist & other .txt file options.

--- CanaData.py
+++ CanaData.py
@@ -44,8 +44,6 @@ class CanaData:
         self.unFriendlyStates = []
         # Set to True if there are no locations
         self.NonGreenState = False
-        # Sets whether or not we grab the slugs for the search
-        self.slugGrab = False
 
     # This function recieves a URL (string) and makes an HTTP request to it
     # If successul, converts the response to JSON and returns the dataset
@@ -446,6 +444,15 @@ class CanaData:
         if len(self.unFriendlyStates) > 0:
             print(f'\nThese States were found to have 0 listings!\n{", ".join(self.unFriendlyStates)}')
 
+    def getSlugs(self, url):
+        stateURL = url
+        stateSlugs = self.do_request(stateURL)
+        region = stateSlugs['data']['subregions'][0]['region_path'].split('/')[0]
+        slugList = []
+        for state in stateSlugs['data']['subregions']:
+            slugList.append(state['slug'])
+        print(f'\nHere are the list of {region.capitalize()} State Slugs!\n{", ".join(slugList)}\n')
+
     # Function to determine if we are searching for Dispensary data or Delivery Data (can be both)
     def identifyDataTypes(self):
         # Ask the user to put y/yes or we wont search dispensaries
@@ -460,11 +467,6 @@ class CanaData:
             # Set self value to False so deliveries are included in datasets
             self.deliveries = False
 
-    # Sets the self attribute for grabbing slugs within a search
-    def slugs(self):
-        print('Set slugGrab to true!')
-        self.slugGrab = True
-
     def TestMode(self):
         print('Set Troubleshooting Mode to True')
         self.testMode = True
@@ -477,24 +479,6 @@ if __name__ == '__main__':
     # This is where we end pu putting our list of items. Replaced with a list of search slugs -> []
     searchSlugs = None
 
-    try:
-        # Grab list of States from local file
-        allStatesSlugs = [line.rstrip('\n').lower().replace(' ', '-') for line in open('states.txt')]  # Updated by Manually through magic
-    except Exception as e:
-        print('Looks like no states.txt file! No biggy, just cant use the all option!')
-
-    try:
-        # Grab list of known Cities from local file
-        knownSlugs = [line.rstrip('\n').lower().replace(' ', '-') for line in open('slugs.txt')]
-    except Exception as e:
-        print('Looks like no slugs.txt file! No biggy, just cant use the slugs option!')
-
-    try:
-        # Grab list of known Cities from local file
-        mySlugList = [line.rstrip('\n').lower().replace(' ', '-') for line in open('mylist.txt')]  # Updated by Manually through magic
-    except Exception as e:
-        print('Looks like no mylist.txt file! No biggy, just cant use the mylist option!')
-
     # Argument list
     argList = list(argv)
 
@@ -505,56 +489,44 @@ if __name__ == '__main__':
     if len(argList) > 1:
         # There were arguments! Now to check for specifics
 
-        # This looks to see if we need to save the City list that we identify!
-        if '-slugs' in argList:
-            cana.slugs()
+        # This looks to see if the user wants to see a list of US Slugs!
+        if '-us' in argList:
+            cana.getSlugs('https://api-g.weedmaps.com/wm/v1/regions/united-states/subregions')
+            exit()
+
+        # This looks to see if the user wants to see a list of Canada Slugs!
+        if '-canada' in argList:
+            cana.getSlugs('https://api-g.weedmaps.com/wm/v1/regions/canada/subregions')
+            exit()
 
     # This specifically looks for the quick run argument and sets the State list
     if '-go' in argList:
         # Search slug location in args is after the -go
         searchSlug = argList.index('-go') + 1
-        # Determine if its one of our preset 3 or a regular search
-        if argv[searchSlug].lower() == 'mylist':
-            searchSlugs = mySlugList
-        elif argv[searchSlug].lower() == 'slugs':
-            # Slug list is set to the list from the cities.txt file
-            searchSlugs = knownSlugs
-        elif argv[searchSlug].lower() == 'all':
-            # Slug list is set to the list from the cities.txt file
-            searchSlugs = allStatesSlugs
-        else:
-            searchSlugs = [argv[searchSlug].lower()]
         # Visual queue of start (in place of question for search slug)
-        print(f'\n\n   !!~~-- Welcome to CanaData  (>-_-)>  --~~!!\n\n\n\nStarting Quickrun on {str(len(searchSlugs))} Slugs: \n{str(", ".join(searchSlugs))}\n\n\n')
+        print(f'\n\n   !!~~-- Welcome to CanaData  (>-_-)>  --~~!!\n\n\n\nStarting Quickrun on {searchSlug}\n\n\n')
 
     # If user is not doing Quickrun
     # Ask them for a slug then determine if its one of our preset 3 or a regular search
     else:
         # Ask the user for what City they'd like to run
-        answer = input(f'\n\n   !!~~-- Welcome to CanaData  (>-_-)>  --~~!!\n\nWhat city slug or state slug would you like to search? Can put all for all states or mylist for your custom list or slugs for the list of custom slugs from slugs.txt!\n\nKnown State Options:\n{", ".join(allStatesSlugs)}\n\nKnown Slug Options:\n{", ".join(knownSlugs)}\n\nKnown Mylist Options:\n{", ".join(mySlugList)}\n\n-- ').lower()
+        print('\n\n   !!~~-- Welcome to CanaData  (>-_-)>  --~~!!\n')
+        # Post US Slugs for User to use:
+        cana.getSlugs('https://api-g.weedmaps.com/wm/v1/regions/united-states/subregions')
+        # Post Canada Slugs for User to use:
+        cana.getSlugs('https://api-g.weedmaps.com/wm/v1/regions/canada/subregions')
+        answer = input('\nWhat state slug would you like to search? Or try your local city in lowercase with - in place of spaces!\n\n-- ').lower()
 
-        # Check if user asked for all
-        if answer == 'all':
-            # States list is set to our 50 state list # Fingers crossed it runs through all!
-            searchSlugs = allStatesSlugs
-        elif answer == 'mylist':
-            # States list is set to the list from the myList.txt file
-            searchSlugs = mySlugList
-        elif answer == 'slugs':
-            # Slug list is set to the list from the cities.txt file
-            searchSlugs = knownSlugs
-        else:
-            # State list is set to a single item list of what the user input
-            searchSlugs = [answer]
+        # State list is set to a single item list of what the user input
+        searchSlug = answer
 
-    # This Loop fires no matter what to process all search slugs provided either manually or through a .txt file!
-    # Fun functions against them all!
-    for slug in searchSlugs:
-        if len(slug) > 0:
+        # This Loop fires no matter what to process all search slugs provided either manually or through a .txt file!
+        # Fun functions against them all!
+        if len(searchSlug) > 0:
             # Visual queue of starting a state
-            print(f'\n\nStarting on {slug}')
+            print(f'\n\nStarting on {searchSlug}')
             # Set our searchSlug to the State we are working on
-            cana.setCitySlug(slug)
+            cana.setCitySlug(searchSlug)
             # Get the locations for the given slug
             cana.getLocations()
             # Get the Menus for the locations found
@@ -563,5 +535,7 @@ if __name__ == '__main__':
             cana.dataToCSV()
             # Reset the self variables to avoid using old data from other states/slugs
             cana.resetDataSets()
+
+
     # Print out the list of Non-Cannabis friendly states
     cana.identifyNaughtyStates()
